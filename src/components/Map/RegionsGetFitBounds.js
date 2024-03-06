70d037b9c92b9e4e2066c9e6b82df3e6e921928d
Andre Farias] <farias96andre@gmail.com>
2024-03-06 15:05:44 -0300

    fix: alterando fonte de dados estatica para dados da api

--- src/components/Map/RegionsGetFitBounds.js
+++ src/components/Map/RegionsGetFitBounds.js
@@ -1,13 +1,13 @@
+import {loadRegionData} from "../../services/loadRegionData";
 
-import Regions from "../../data/Regions"
+export default async function RegionsGetFitBounds(){
+    const Regions = await loadRegionData();
 
- export default function RegionsGetFitBounds(){
     const allRegionsBounds = []
     let markerTopLeft = [0,-90]
     let markerBotRight = [-179, 90]
 
     const regionBounds = []
-
     Regions.forEach( item => {
         let markerTLR = [0,-90]
         let markerBRR = [-179,90]
@@ -27,11 +27,11 @@ import Regions from "../../data/Regions"
                 if(path[0] > markerBRR[0]){ markerBRR[0] = path[0]};
                 if(path[1] < markerBRR[1]){ markerBRR[1] = path[1]};
             })
-            
+
         })
-        regionBounds.push([{lat:markerTLR[1], lng:markerTLR[0]},{lat:markerBRR[1], lng:markerBRR[0]}]) 
+        regionBounds.push([{lat:markerTLR[1], lng:markerTLR[0]},{lat:markerBRR[1], lng:markerBRR[0]}])
     })
 
     allRegionsBounds.push({lat: markerTopLeft[1], lng:markerTopLeft[0]},{lat:markerBotRight[1], lng:markerBotRight[0]})
     return {allRegionsBounds, regionBounds}
- }
+}
